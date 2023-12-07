0d3c3ba35f13956989e262dcfc3a556087397665
mirsery <mirsery@163.com>
2023-12-07 14:51:01 +0800

    update the code

--- app/src/main/java/com/mirsery/screenfree/ADActivity.kt
+++ app/src/main/java/com/mirsery/screenfree/ADActivity.kt
@@ -4,9 +4,7 @@ import android.content.pm.ActivityInfo
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowManager
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowInsetsCompat
 import com.mirsery.screenfree.widget.simple.SimpleADWidget
